72ffffe32f15331855bdb965b75db35ef3a49d29
Jpoland0202 <45803275+Jpoland0202@users.noreply.github.com>
2020-08-24 13:59:31 -0500

    Initial commit

---
+++ matrixRain.bat
@@ -0,0 +1,6 @@
+@echo off
+ color 02
+ title Matrix Rain
+ :start
+ echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% .
+ goto start
